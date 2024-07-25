e59bcb6c111b30806f7a36358e0a302cd1132850
Maldonado <joseromaldo.1997@gmail.com>
2024-07-25 08:49:02 -0600

    Creación de model Conexion y funciones js

---
+++ models/Conexion.php
@@ -0,0 +1,65 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+abstract class Conexion {
+
+    protected static $conexion = null;
+
+    private static function conectar(){
+        try{
+            self::$conexion = new PDO('informix:host=host.docker.internal; service=9088; database=company; server=informix; protocol=onsoctcp;EnableScrollableCursors = 1','informix','in4mix'); 
+            // DEFINIR EL MANEJO DE EXCEPCIONES
+            self::$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            //echo "CONEXION EXITOSA";
+        }catch(PDOException $e){
+            // IMPRIME EN PANTALLA EL ERROR
+            echo "Error de conexion en la Base de Datos";
+            echo "<br>";
+            echo $e->getMessage();
+            exit;
+        }
+
+        return self::$conexion;
+ 
+    }
+    //Metodos para las sentencias sql
+
+    public static function ejecutar($sql){
+        // CONECTANDOSE A LA BD CON EL METODO CONECTAR
+        self::conectar();
+        // PREPARAMOS LA SENTENCIA
+        $sentencia = self::$conexion->prepare($sql);
+        // EJECUTAMOS A SENTENCIA
+        $resultado = $sentencia->execute();
+        $id = self::$conexion->lastInsertId();
+        // CERRANDO LA CONEXION
+        self::$conexion = null;
+        // DEVOLVEMOS RESULTADOS
+        return [
+            'resultado' => $resultado,
+            'id' => $id
+        ];
+    }
+    //Metodo para consultar infomación
+
+    public static function servir($sql){
+        // CONECTANDOSE A LA BD CON EL METODO CONECTAR
+        self::conectar();
+        // PREPARAMOS LA SENTENCIA
+        $sentencia = self::$conexion->prepare($sql);
+        // EJECUTAMOS A SENTENCIA
+   
+        $sentencia->execute();
+        $resultados = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+        
+
+        // CERRANDO LA CONEXION
+        self::$conexion = null;
+        
+        // DEVOLVEMOS RESULTADOS
+        return $resultados;
+    }
+  
+}
+
--- views/grado/index.php
+++ views/grado/index.php
@@ -3,22 +3,47 @@
   <h1 class="text-center mb-4 text-black">Grado Oficiales</h1>
   <div class="row justify-content-center">
     <form id="formulario" class="col-lg-8 bg-white p-4 shadow rounded">
+    <input type="hidden" name="gra_id" id="gra_id">
       <div class="mb-3">
         <label for="gra_nombre" class="form-label">Ingrese un grado</label>
         <input type="text" name="gra_nombre" id="gra_nombre" class="form-control" placeholder="Ej. Capitán" required>
       </div>
       <div class="row">
       <div class="col d-grid">
-        <button type="submit" class="btn btn-primary btn-lg"><i class="bi bi-floppy me-2"></i>Guardar</button>
+        <button type="submit" id="btnGuardar" class="btn btn-primary btn-lg"><i class="bi bi-floppy me-2"></i>Guardar</button>
       </div>
       <div class="col d-grid">
-        <button type="submit" class="btn btn-info btn-lg"><i class="bi bi-search me-3"></i>Buscar</button>
+        <button type="submit" id="btnBuscar" class="btn btn-info btn-lg"><i class="bi bi-search me-3"></i>Buscar</button>
       </div>
       <div class="col d-grid">
-        <button type="submit" class="btn btn-warning btn-lg"><i class="bi bi-trash me-2"></i>Limpiar</button>
+        <button type="submit" id="btnLimpiar" class="btn btn-warning btn-lg"><i class="bi bi-trash me-2"></i>Limpiar</button>
       </div>
       </div>
     </form>
   </div>
+
+  <div class="row justify-content-center">
+        <div class="col-lg-8 table-responsive">
+            <h2 class="text-center mt-3">Listado de grados</h2>
+            <table class="table table-bordered table-hover" id="tablaGrados">
+                <thead class="thead-dark">
+                    <tr>
+                        <th>No.</th>
+                        <th>Nombre</th>
+                        <th>Modificar</th>
+                        <th>Eliminar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td colspan="5" class="text-center">No hay grados disponibles</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
 </div>
+<script defer src="/formaD_conJS/src/js/funciones.js"></script>
+<script defer src="/formaD_conJS/src/js/grado/index.js"></script>
 <?php include_once '../../includes/footer.php' ?>
