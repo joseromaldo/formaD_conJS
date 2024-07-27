c80b38be0e4b3b4140b11a3f852994a8bac6a7db
Maldonado <joseromaldo.1997@gmail.com>
2024-07-26 23:53:44 -0600

    Modificacion para detalle

---
+++ src/js/detalle/index.js
@@ -0,0 +1,372 @@
+const btnGuardar = document.getElementById('btnGuardar');
+const btnModificar = document.getElementById('btnModificar');
+const btnBuscar = document.getElementById('btnBuscar');
+const btnCancelar = document.getElementById('btnCancelar');
+const btnLimpiar = document.getElementById('btnLimpiar');
+const tablaAsigs = document.getElementById('tablaAsigs');
+const formulario = document.getElementById('formulario');
+
+btnModificar.parentElement.style.display = 'none';
+btnCancelar.parentElement.style.display = 'none';
+formulario.parentElement.style.display = 'none';
+
+
+
+const getAsigs = async (alerta ='si') => {
+    const pro = formulario.asi_pro.value.trim();
+    const app = formulario.asi_app.value.trim();
+    
+  
+    const url = `/formaD_conJS/controllers/asignar/index.php?asi_pro=${pro}&asi_app=${app}`;
+  
+    const config = {
+        method: 'GET'
+    };
+
+    try {
+        const respuesta = await fetch(url, config);
+        const data = await respuesta.json();
+        console.log(data);
+       
+        tablaAsigs.tBodies[0].innerHTML = '';
+        const fragment = document.createDocumentFragment();
+        let contador = 1;
+
+        if (respuesta.status === 200) {
+            if(alerta =='si'){
+                Swal.mixin({
+                    toast: true,
+                    position: "top-end",
+                    showConfirmButton: false,
+                    timer: 3000,
+                    timerProgressBar: true,
+                    icon: "success",
+                    title: 'Asignaciones encontradas',
+                    didOpen: (toast) => {
+                        toast.onmouseenter = Swal.stopTimer;
+                        toast.onmouseleave = Swal.resumeTimer;
+                    }
+                }).fire();
+
+            }
+
+            if (data.length > 0) {
+                data.forEach(asi => {
+                    const tr = document.createElement('tr');
+                    const celda1 = document.createElement('td');
+                    const celda2 = document.createElement('td');
+                    const celda3 = document.createElement('td');
+                    const celda4 = document.createElement('td');
+                    const celda5 = document.createElement('td');
+                    const celda6 = document.createElement('td');
+                   
+                    const buttonEliminar = document.createElement('button');
+
+                    celda1.innerText = contador;
+                    celda2.innerText = asi.PRO_NOMBRE;
+                    celda3.innerText = asi.PRO_APELLIDO;
+                    celda4.innerText = asi.APP_NOMBRE;                 
+
+                
+                    buttonEliminar.textContent = 'Eliminar';
+                    buttonEliminar.classList.add('btn', 'btn-danger', 'w-100');
+                    buttonEliminar.addEventListener('click', () => eliminar(asi));
+
+                  
+                    celda5.appendChild(buttonEliminar);
+
+                    tr.appendChild(celda1);
+                    tr.appendChild(celda2);
+                    tr.appendChild(celda3);
+                    tr.appendChild(celda4);
+                    tr.appendChild(celda5);
+                  
+                  
+                    fragment.appendChild(tr);
+
+                    contador++;
+                });
+            } else {
+                const tr = document.createElement('tr');
+                const td = document.createElement('td');
+                td.innerText = 'No hay asignaciones disponibles';
+                td.colSpan = 4 ;
+
+                tr.appendChild(td);
+                fragment.appendChild(tr);
+            }
+        } else {
+            console.log('Error al cargar ');
+        }
+
+        tablaAsigs.tBodies[0].appendChild(fragment);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+
+const guardarAsig = async (e) => {
+    e.preventDefault();
+    btnGuardar.disabled = true;
+
+    const url = '/formaD_conJS/controllers/asignar/index.php';
+    const formData = new FormData(formulario);
+    formData.append('tipo', 1);
+    formData.delete('asi_id');
+
+    const config = {
+        method: 'POST',
+        body: formData
+    };
+
+    try {
+        console.log('Enviando datos:', ...formData.entries());
+        const respuesta = await fetch(url, config);
+        const data = await respuesta.json();
+        console.log('Respuesta recibida:', data);
+        const { mensaje, codigo, detalle } = data;
+        if (respuesta.ok && codigo === 1) {
+            Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                icon: "success",
+                title: mensaje,
+                didOpen: (toast) => {
+                    toast.onmouseenter = Swal.stopTimer;
+                    toast.onmouseleave = Swal.resumeTimer;
+                }
+            }).fire();
+
+            formulario.reset();
+            getAsigs(alerta ='no');
+        } else {
+            console.log('Error:', detalle);
+            Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                icon: "error",
+                title: 'Error al guardar',
+                didOpen: (toast) => {
+                    toast.onmouseenter = Swal.stopTimer;
+                    toast.onmouseleave = Swal.resumeTimer;
+                }
+            }).fire();
+        }
+    } catch (error) {
+        console.log('Error de conexión:', error);
+        Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            icon: "error",
+            title: 'Error de conexión',
+            didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+            }
+        }).fire();
+    }
+    btnGuardar.disabled = false;
+};
+
+const llenarDatos = (asi) => {
+
+    formulario.asi_id.value = asi.ASI_ID;
+    formulario.asi_pro.value = asi.PRO_ID;
+    formulario.asi_app.value = asi.APL_ID;
+   
+   
+
+    btnLimpiar.parentElement.style.display = 'none';
+    btnBuscar.parentElement.style.display = 'none';
+    btnCancelar.parentElement.style.display = '';
+    btnGuardar.parentElement.style.display = 'none';
+    btnModificar.parentElement.style.display = '';
+};
+
+const modificar = async (e) => {
+    e.preventDefault();
+    btnModificar.disabled = true;
+
+    const url = '/formaD_conJS/controllers/asignar/index.php';
+    const formData = new FormData(formulario);
+    formData.append('tipo', 2);
+
+    const config = {
+        method: 'POST',
+        body: formData
+    };
+
+    try {
+        console.log('Enviando datos:', ...formData.entries());
+        const respuesta = await fetch(url, config);
+        const data = await respuesta.json();
+        console.log('Respuesta recibida:', data);
+        const { mensaje, codigo, detalle } = data;
+        if (respuesta.ok && codigo === 1) {
+            Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                icon: "success",
+                title: mensaje,
+                didOpen: (toast) => {
+                    toast.onmouseenter = Swal.stopTimer;
+                    toast.onmouseleave = Swal.resumeTimer;
+                }
+            }).fire();
+
+            formulario.reset();
+            getAsigs(alerta ='no');
+            btnBuscar.parentElement.style.display = '';
+            btnCancelar.parentElement.style.display = 'none';
+            btnGuardar.parentElement.style.display = '';
+            btnModificar.parentElement.style.display = 'none';
+        } else {
+            console.log('Error:', detalle);
+            Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                icon: "error",
+                title: 'Error al modificar',
+                didOpen: (toast) => {
+                    toast.onmouseenter = Swal.stopTimer;
+                    toast.onmouseleave = Swal.resumeTimer;
+                }
+            }).fire();
+        }
+    } catch (error) {
+        console.log('Error de conexión:', error);
+        Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            icon: "error",
+            title: 'Error de conexión',
+            didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+            }
+        }).fire();
+    }
+    btnModificar.disabled = false;
+};
+
+const cancelar = (e) => {
+    e.preventDefault();
+
+    formulario.reset();
+
+    btnLimpiar.parentElement.style.display = '';
+    btnBuscar.parentElement.style.display = '';
+    btnCancelar.parentElement.style.display = 'none';
+    btnGuardar.parentElement.style.display = '';
+    btnModificar.parentElement.style.display = 'none';
+
+    Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        icon: "info",
+        title: 'Modificación cancelada',
+        didOpen: (toast) => {
+            toast.onmouseenter = Swal.stopTimer;
+            toast.onmouseleave = Swal.resumeTimer;
+        }
+    }).fire();
+};
+
+const eliminar = async (asiId) => {
+    const url = '/formaD_conJS/controllers/asignar/index.php';
+    const formData = new FormData();
+    formData.append('asi_id', asiId);
+    formData.append('tipo', 3);
+
+    const config = {
+        method: 'POST',
+        body: formData
+    };
+
+    try {
+        const respuesta = await fetch(url, config);
+        const data = await respuesta.json();
+        const { mensaje, codigo, detalle } = data;
+        if (respuesta.ok && codigo === 1) {
+            Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                icon: "success",
+                title: mensaje,
+                didOpen: (toast) => {
+                    toast.onmouseenter = Swal.stopTimer;
+                    toast.onmouseleave = Swal.resumeTimer;
+                }
+            }).fire();
+
+            getAsigs(alerta ='no');
+        } else {
+            console.log('Error:', detalle);
+            Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                icon: "error",
+                title: 'Error al eliminar',
+                didOpen: (toast) => {
+                    toast.onmouseenter = Swal.stopTimer;
+                    toast.onmouseleave = Swal.resumeTimer;
+                }
+            }).fire();
+        }
+    } catch (error) {
+        console.log('Error de conexión:', error);
+        Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            icon: "error",
+            title: 'Error de conexión',
+            didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+            }
+        }).fire();
+    }
+};
+
+const limpiarFormulario = () => {
+    formulario.reset();
+};
+
+getAsigs();
+btnGuardar.addEventListener('click', guardarAsig);
+btnModificar.addEventListener('click', modificar);
+btnCancelar.addEventListener('click', cancelar);
+btnBuscar.addEventListener('click', getAsigs);
+btnLimpiar.addEventListener('click', limpiarFormulario);
--- views/detalle/index.php
+++ views/detalle/index.php
@@ -1,13 +1,103 @@
-<?php include_once '../../includes/header.php'?>
+<?php include_once '../../includes/header.php' ?>
 <?php
+require '../../models/Aplicacion.php';
+require '../../models/Programador.php';
+try {
 
-require '../../models/Asignar.php';
-$nombre = new Asignar();
+  $aplicacion = new Aplicacion();
 
-$nombres = $nombre->buscar();
+  $Aplicaciones = $aplicacion->buscar();
+} catch (PDOException $e) {
+  $error = $e->getMessage();
+} catch (Exception $e2) {
+  $error = $e2->getMessage();
+}
+
+
+try {
+
+  $programador = new Programador();
+
+
+  $programadores = $programador->buscar();
+  
+
+} catch (PDOException $e) {
+  $error = $e->getMessage();
+} catch (Exception $e2) {
+  $error = $e2->getMessage();
+}
 
 ?>
 
+<div class="container mt-5">
+  <h1 class="text-center mt-3">DETALLES</h1>
+  <div class="row justify-content-center mt-2">
+    <form id="formulario" class="border border-primary rounded p-3 bg-light col-md-6">
+    <input type="hidden" name="asi_id" id="asi_id">  
 
+    <div class="form-group">
+        <label for="aplicacion">Seleccione un programador</label>
+        <select class="form-select" name="asi_pro" id="asi_pro" required>
+          <option value="">Seleccionar..</option>
+          <?php foreach ($programadores as $programador) { ?>
+            <option value="<?php echo $programador['PRO_ID']; ?>"><?php echo $programador['PRO_NOMBRE']; ?></option>
+          <?php } ?>
+        </select>
+      </div>
 
-<?php include_once '../../includes/footer.php'?>
+    <div class="form-group">
+        <label for="asi_app">Seleccione una aplicación:</label>
+        <select class="form-select" name="asi_app" id="asi_app" required>
+          <option value="">Seleccionar..</option>
+          <?php foreach ($Aplicaciones as $aplicacion1) { ?>
+            <option value="<?php echo $aplicacion1['APL_ID']; ?>"><?php echo $aplicacion1['APL_NOMBRE']; ?></option>
+          <?php } ?>
+        </select>
+      </div>
+   
+      <div class="row mt-3">
+                                    <div class="col">
+                                        <button type="submit" id="btnGuardar" class="btn btn-primary w-100">Guardar</button>
+                                    </div>
+                                    <div class="col">
+                                        <button type="button" id="btnBuscar" class="btn btn-info w-100">Buscar</button>
+                                    </div>
+                                    <div class="col">
+                                        <button type="button" id="btnModificar" class="btn btn-secondary w-100">Modificar</button>
+                                    </div>
+                                    <div class="col">
+                                        <button type="button" id="btnCancelar" class="btn btn-danger w-100">Cancelar</button>
+                                    </div>
+                                    <div class="col">
+                                        <button type="reset" id="btnLimpiar" class="btn btn-warning w-100">Limpiar</button>
+                                    </div>
+                                </div>
+    </form>
+  </div>
+  <div class="row justify-content-center">
+        <div class="col-lg-12 table-responsive">
+            <h2 class="text-center mt-3">Listado programadores encargados de apps</h2>
+            <table class="table table-bordered table-hover" id="tablaAsigs">
+                <thead class="thead-dark">
+                    <tr>
+                        <th>No.</th>
+                        <th>Programador</th>
+                        <th>Apellido</th>
+                        <th>App</th>
+                        <th>Eliminar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td colspan="6" class="text-center">No hay asignaciones disponibles</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<script defer src="/formaD_conJS/src/funciones.js"></script>
+<script defer src="/formaD_conJS/src/js/detalle/index.js"></script>
+
+<?php include_once '../../includes/footer.php' ?>
